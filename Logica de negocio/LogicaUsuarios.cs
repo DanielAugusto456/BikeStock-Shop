21d49b79ff03adee8c7aacc9628a975ddcde4387
Daniel A <martinezapata809@gmail.com>
2026-04-15 15:49:19 -0400

    Logicca de usuarios

---
+++ Logica de negocio/LogicaUsuarios.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica_de_negocio
+{
+    public class LogicaUsuarios
+    {
+        Datos.UserRepository repositorio = new Datos.UserRepository();
+
+        public void IngresarUsuario(string username, string password)
+        {
+            repositorio.IngresarUsuario(username, password);
+        }
+
+        public bool ValidarUsuario(string username, string password)
+        {
+            return repositorio.ValidarUsuario(username, password);
+        }
+
+        public void ActualizarUsuario(int id, string username, string password)
+        {
+            repositorio.ActualizarUsuario(id, username, password);
+        }
+
+        public void EliminarUsuario(int id)
+        {
+            repositorio.EliminarUsuario(id);
+        }
+    }
+}
